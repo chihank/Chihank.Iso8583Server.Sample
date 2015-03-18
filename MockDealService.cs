1c0a87fb1ec6c763b86bb2fed9619e907a3c31dc
moon.wa <moon.ww.z@gmail.com>
2015-03-18 14:55:33 +0800

    init

---
+++ MockDealService.cs
@@ -0,0 +1,126 @@
+using System;
+using Chihank.Iso8583Server.Requests;
+
+namespace Chihank.Iso8583Server.Sample
+{
+    public class MockDealService : IDealService
+    {
+        public Terminal GetTerminal(String terminalId, String tenantId)
+        {
+            Console.WriteLine(String.Format("get terminal"));
+            Console.WriteLine(String.Format("tenantId: {0}", tenantId));
+            Console.WriteLine(String.Format("terminalId: {0}", terminalId));
+
+            Terminal terminal = new Terminal
+            {
+                TenantId = tenantId,
+                TerminalId = terminalId,
+                EncryptKey = "3131313131313131"
+            };
+            return terminal;
+        }
+
+        public Tenant GetTenant(String tenantId)
+        {
+            Console.WriteLine(String.Format("get tenant"));
+            Console.WriteLine(String.Format("tenantId: {0}", tenantId));
+
+            Tenant tenant = new Tenant();
+            tenant.TenantId = tenantId;
+            return tenant;
+        }
+
+        public TransportationResponse AmountPayment(IDealRequest request)
+        {
+            Console.WriteLine(String.Format("amountPayment"));
+            Console.WriteLine(String.Format("account name: {0}", request.AccountName));
+            Console.WriteLine(String.Format("account token: {0}", request.AccountToken));
+            Console.WriteLine(String.Format("account amount: {0}", request.Amount));
+            Console.WriteLine(String.Format("account batch no: {0}", request.BatchNo));
+            Console.WriteLine(String.Format("password: {0}", request.Password));
+            Console.WriteLine(String.Format("serial no: {0}", request.SerialNo));
+            Console.WriteLine(String.Format("tenantId: {0}", request.TenantId));
+            Console.WriteLine(String.Format("terminalId: {0}", request.TerminalId));
+
+            TransportationResponse rsp = new TransportationResponse();
+            rsp.ResponseCode = ResponseCode.BadAccount;
+            rsp.ReferenceNo = "000000000001";
+            rsp.Amount = 1;
+            return rsp;
+        }
+
+        public TransportationResponse PointPayment(IDealRequest request)
+        {
+            Console.WriteLine(String.Format("pointPayment"));
+            Console.WriteLine(String.Format("account name: {0}", request.AccountName));
+            Console.WriteLine(String.Format("account token: {0}", request.AccountToken));
+            Console.WriteLine(String.Format("account amount: {0}", request.Amount));
+            Console.WriteLine(String.Format("account batch no: {0}", request.BatchNo));
+            Console.WriteLine(String.Format("password: {0}", request.Password));
+            Console.WriteLine(String.Format("serial no: {0}", request.SerialNo));
+            Console.WriteLine(String.Format("tenantId: {0}", request.TenantId));
+            Console.WriteLine(String.Format("terminalId: {0}", request.TerminalId));
+
+            TransportationResponse rsp = new TransportationResponse();
+            rsp.ResponseCode = ResponseCode.Success;
+            rsp.ReferenceNo = "000000000005";
+            rsp.Amount = 5;
+            return rsp;
+        }
+
+        public TransportationResponse AmountQuery(IDealRequest request)
+        {
+            Console.WriteLine(String.Format("amount query"));
+            Console.WriteLine(String.Format("account name: {0}", request.AccountName));
+            TransportationResponse rsp = new TransportationResponse();
+            rsp.ResponseCode = ResponseCode.Success;
+            rsp.ReferenceNo = "000000000002";
+            rsp.Amount = 2;
+            return rsp;
+        }
+
+
+        public TransportationResponse PointQuery(IDealRequest request)
+        {
+            Console.WriteLine(String.Format("point query"));
+            Console.WriteLine(String.Format("account name: {0}", request.AccountName));
+            TransportationResponse rsp = new TransportationResponse();
+            rsp.ResponseCode = ResponseCode.Success;
+            rsp.ReferenceNo = "000000000007";
+            rsp.Amount = 7;
+            return rsp;
+        }
+        public TransportationResponse CancelPayment(IDealRequest request)
+        {
+            Console.WriteLine(String.Format("cancelPayment"));
+            Console.WriteLine(String.Format("account name: {0}", request.AccountName));
+            Console.WriteLine(String.Format("account token: {0}", request.AccountToken));
+            Console.WriteLine(String.Format("account amount: {0}", request.Amount));
+            Console.WriteLine(String.Format("account batch no: {0}", request.BatchNo));
+            Console.WriteLine(String.Format("original serial no: {0}", request.OriginalSerialNo));
+            Console.WriteLine(String.Format("password: {0}", request.Password));
+            Console.WriteLine(String.Format("serial no: {0}", request.SerialNo));
+            Console.WriteLine(String.Format("tenantId: {0}", request.TenantId));
+            Console.WriteLine(String.Format("terminalId: {0}", request.TerminalId));
+            TransportationResponse rsp = new TransportationResponse();
+            rsp.ResponseCode = ResponseCode.Success;
+            rsp.ReferenceNo = "000000000003";
+            rsp.Amount = 3;
+            return rsp;
+        }
+
+        public TransportationResponse Rollback(IRollbackRequest request)
+        {
+            Console.WriteLine(String.Format("rollback"));
+            Console.WriteLine(String.Format("account batch no: {0}", request.BatchNo));
+            Console.WriteLine(String.Format("serial no: {0}", request.SerialNo));
+            Console.WriteLine(String.Format("tenantId: {0}", request.TenantId));
+            Console.WriteLine(String.Format("terminalId: {0}", request.TerminalId));
+            TransportationResponse rsp = new TransportationResponse();
+            rsp.ResponseCode = ResponseCode.Success;
+            rsp.ReferenceNo = "000000000004";
+            rsp.Amount = 4;
+            return rsp;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Chihank.Network;
+
+namespace Chihank.Iso8583Server.Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 监听端口 3130
+            var service = new Iso8583Service(3130, new MockDealService());
+            // 记日志
+            service.Logger = new ConsoleLogger();
+            // 设置空闲断开链接时间
+            service.IdleToCloseClient = TimeSpan.FromSeconds(10);
+            // 设置持久化通信密钥存储
+            service.TenantService = new MemoryTenantService();
+            // 启动服务
+            service.Start();
+
+            Console.WriteLine("press any key to stop");
+            Console.ReadKey();
+            service.Stop();
+        }
+    }
+
+    internal class ConsoleLogger : ILogger
+    {
+        public void WriteLog(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public void WriteDebug(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public void WriteError(string message, Exception exception)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
